742e925a68cac7093a6d0c4998142967d971dd08
Medkelawit_Abadina <mekdelawitkassa6@gmail.com>
2025-09-14 15:19:08 +0300

    the last project

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ['title', 'author', 'publication_year']
+    
+    # Enable filtering by publication year and author
+    list_filter = ['publication_year', 'author']
+    
+    # Enable search functionality
+    search_fields = ['title', 'author']
+    
+    # Fields to display in the detail/edit form
+    fields = ['title', 'author', 'publication_year']
+    
+    # Optional: Add ordering
+    ordering = ['title']  # Books will be ordered by title by default
